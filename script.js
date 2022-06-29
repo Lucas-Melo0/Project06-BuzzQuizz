703de5d0767062271259c54d650a2d56ce142ad2
LucasM <lucassss8v@gmail.com>
2022-06-29 11:08:21 -0300

    Finished Nav button 'Make Quizz'

---
+++ script.js
@@ -0,0 +1,8 @@
+function openQuizzCreatorPage (){
+    hideHomePage();
+
+}
+function hideHomePage (){
+    let homePage = document.querySelector(".home-page")
+    homePage.classList.add("hidden")
+}
